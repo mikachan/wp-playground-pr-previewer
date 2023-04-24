7f3768451808ac70842ca11f5d06f5c95e24f5b2
Sarah Norris <sarah@sekai.co.uk>
2023-04-24 20:15:05 +0100

    Attempt to run node script

--- playground-previewer.js
+++ playground-previewer.js
@@ -1,6 +1,6 @@
 import { connectPlayground, login, installTheme } from '@wp-playground/client';
 
-const themeZip = process.argv[ 0 ];
+const themeZipUrl = process.argv[ 0 ];
  
 const client = await connectPlayground(
     // Any iframe:
@@ -15,12 +15,14 @@ await client.isReady();
 await login(client, 'admin', 'password');
  
 // Implement fetchFromPr and make it return a File object
-// const themeZip = await fetchFromPr( 510 );
+const themeZip = await fetchFromPr( themeZipUrl );
  
 // Install the theme
 await installTheme( themeZip );
 
-// async function fetchFromPr( prNumber ) {
-//     // Fetch the PR zip from GitHub
-//     // Return a File object
-// }
+async function fetchFromPr( url ) {
+    const response = await fetch( url );
+    const blob = await response.blob();
+    const filename = url.substring( url.lastIndexOf('/') + 1 );
+    return new File( [blob], filename );
+}
