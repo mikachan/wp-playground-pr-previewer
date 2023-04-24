2d33052890bfe69583730581bfd4dde50b8748db
Sarah Norris <sarah@sekai.co.uk>
2023-04-24 17:32:37 +0100

    Try passing args

--- handle-theme-zip.sh
+++ handle-theme-zip.sh
@@ -2,8 +2,14 @@
 
 handleThemeZip=()
 
+pr_sha="$1"
+sha="$2"
+
+echo $1
+echo $2
+
 for theme in $(find * -type d); do
-    if ...; then # check if $theme contains changes
+    if git diff --name-only --diff-filter=ACMRT ${{ pr_sha }} ${{ sha }} | grep -c "^$i/"; then
         for e in "${handleThemeZip[@]}"; do
             [[ "$theme" == "$e"* ]] && continue 2
         done
