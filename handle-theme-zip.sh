fea10dcdda95ef57d8a793a28368bff05c7fdd75
Sarah Norris <sarah@sekai.co.uk>
2023-04-24 17:48:07 +0100

    Try different git diff command

--- handle-theme-zip.sh
+++ handle-theme-zip.sh
@@ -6,7 +6,6 @@ pr_hash="$1"
 current_hash="$2"
 
 for theme in $(find * -type d); do
-    echo $i
     if git diff --name-only --diff-filter=ACMRT $pr_hash $current_hash | grep -c "^$i/"; then
         for e in "${handleThemeZip[@]}"; do
             [[ "$theme" == "$e"* ]] && continue 2
